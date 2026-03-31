6c98ba7fcddb0b0b9dc9bedf5b24cd8152f75051
campusbites75 <campusbites75@gmail.com>
2026-03-31 23:18:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://127.0.0.1:5500",
-  "https://69cbfdd5eaeffa0008188785--campusbitessinghcafe.netlify.app"
+  "https://campusbitessinghcafe.netlify.app",
+  "https://campusbitessinghadmin.netlify.app"
 ];
 
 app.use(
